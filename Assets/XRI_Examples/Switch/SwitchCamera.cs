3fe5c2a80cf578193080038b582363e31f58ec6c
Natalie Cheng <nataliec6041@gmail.com>
2024-04-21 23:54:59 -0500

    camera switching completed, still some small issue

---
+++ Assets/XRI_Examples/Switch/SwitchCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+  public GameObject Camera1;
+  public GameObject Camera2;
+  public int Manager;
+
+  public void ChangeCamera() {
+    GetComponent<Animator>().SetTrigger("Change");
+  }
+
+  public void ManageCamera() {
+    if (Manager == 0) {
+        Cam2();
+        Manager = 1;
+    } else {
+        Cam1();
+        Manager = 0;
+    }
+  }
+  
+
+  void Cam1() {
+    Camera1.SetActive(true);
+    Camera2.SetActive(false);
+
+  }
+
+  void Cam2() {
+    Camera1.SetActive(false);
+    Camera2.SetActive(true);
+  }
+}
