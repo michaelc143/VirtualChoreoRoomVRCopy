ed4cc75595041195cfbd1427f214a458760de08a
Sydney Scalzo <sscalzo@wisc.edu>
2024-04-29 21:42:49 -0500

    tentative dancer movement

--- Assets/XRI_Examples/SocketInteractors/Scripts/dancer.cs
+++ Assets/XRI_Examples/SocketInteractors/Scripts/dancer.cs
@@ -11,9 +11,62 @@ public class dancer : MonoBehaviour
     public void setPositionData(List<float[]> positions)
     {
         dancerPositions = positions;
-        formationNum = 1;
-        Debug.Log("success! I think");
+        formationNum = 0;
         // Do something with the data, e.g., assign it to a variable
+    }
+
+    public void goToNext(int stage) {
+
+        Debug.Log("new stage : " + stage);
+
+        //make sure you don't exceed formation num
+        if (stage < dancerPositions.Count) {
+
+            //get new and old positions
+            Vector3 newPosition = new Vector3(dancerPositions[stage][0], dancerPositions[stage][1], dancerPositions[stage][2]);
+            //Vector3 oldPosition = new Vector3(dancerPositions[stage-1][0], dancerPositions[stage-1][1], dancerPositions[stage-1][2]);
+
+            float speed = 5f;
+
+            while (transform.position != newPosition) {
+                transform.position = Vector3.MoveTowards(transform.position, newPosition, speed * Time.deltaTime);
+
+            }
+            
+
+            formationNum = stage;
+
+        }
+      
+
+        
+    }
+
+    public void goToPrev(int stage) {
+
+        Debug.Log("new stage : " + stage);
+
+        //make sure you don't exceed formation num
+        if (stage > 0) {
+
+            //get new and old positions
+            Vector3 newPosition = new Vector3(dancerPositions[stage][0], dancerPositions[stage][1], dancerPositions[stage][2]);
+            //Vector3 oldPosition = new Vector3(dancerPositions[stage-1][0], dancerPositions[stage-1][1], dancerPositions[stage-1][2]);
+
+            float speed = 5f;
+
+            while (transform.position != newPosition) {
+                transform.position = Vector3.MoveTowards(transform.position, newPosition, speed * Time.deltaTime);
+
+            }
+            
+
+            formationNum = stage;
+
+        }
+      
+
+        
     }
 
 
